d5af94f057faef017df1a6e3a8dfdfa72e7fdcd1
peter muyambango <muyambangopeter@gmail.com>
2024-02-02 12:52:55 +0200

    worked on screen when cart is empty

--- src/pages/cart/index.tsx
+++ src/pages/cart/index.tsx
@@ -1,7 +1,30 @@
-import React from 'react'
+import { Link } from 'react-router-dom'
+import styles from './index.module.scss'
 
 const Cart = () => {
-  return <div>cart page</div>
+  const itemsInCart = []
+  return (
+    <div className={styles.container}>
+      {itemsInCart.length === 0 ? (
+        <div className={styles.emptyCart}>
+          <h5>Your Shopping Cart is Empty</h5>
+          <p>
+            Your Shopping Cart lives to serve. Give it purpose by adding
+            different products such as laptops, phones and other types of
+            electronics to it. Or visit the{' '}
+            <Link className={styles.link} to={'/'}>
+              homepage
+            </Link>{' '}
+            for all deals
+          </p>
+        </div>
+      ) : (
+        <div className={styles.cart}>
+          <h6>some items in cart</h6>
+        </div>
+      )}
+    </div>
+  )
 }
 
 export default Cart
