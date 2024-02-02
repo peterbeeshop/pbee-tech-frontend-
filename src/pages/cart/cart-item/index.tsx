22f27e9b606e1b15d507e8a4584c8344fefdde32
peter muyambango <muyambangopeter@gmail.com>
2024-02-02 14:45:20 +0200

    worked on the cart screen

---
+++ src/pages/cart/cart-item/index.tsx
@@ -0,0 +1,50 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import { ProductCardType } from '../../../types'
+import styles from './index.module.scss'
+
+type CartItemType = {
+  cartItem: ProductCardType[]
+}
+
+const CartItem = ({ cartItem }: CartItemType) => {
+  return (
+    <>
+      {cartItem.map((item) => {
+        const { image, name, price, links } = item
+        return (
+          <div className={styles.container}>
+            <img src={image} alt="cart-item-img" />
+            <div>
+              <h5>{name}</h5>
+              <h6>$ {price}</h6>
+              <section>
+                <select name="cart-values" id="cart-values">
+                  <option value="0">0 (Delete)</option>
+                  <option value="1" selected>
+                    Quantity: 1
+                  </option>
+                  <option value="2">Quantity: 2</option>
+                  <option value="3">Quantity: 3</option>
+                  <option value="4">Quantity: 4</option>
+                  <option value="5">Quantity: 5</option>
+                </select>
+                <div className={styles.linkContainer}>
+                  <Link className={styles.link} to={'#'}>
+                    Delete
+                  </Link>
+                  <p>|</p>
+                  <Link className={styles.link} to={'#'}>
+                    Add to Watchlist
+                  </Link>
+                </div>
+              </section>
+            </div>
+          </div>
+        )
+      })}
+    </>
+  )
+}
+
+export default CartItem
--- src/pages/cart/index.tsx
+++ src/pages/cart/index.tsx
@@ -1,8 +1,24 @@
 import { Link } from 'react-router-dom'
 import styles from './index.module.scss'
+import phoneImage from '../../assets/dummy-pics/phoneImage.png'
+import CartItem from './cart-item'
 
 const Cart = () => {
-  const itemsInCart = []
+  const itemsInCart = [
+    {
+      name: 'iphone 6',
+      price: 200,
+      image: phoneImage,
+      quantity: 1,
+    },
+    {
+      name: 'iphone 7 plus',
+      price: 400,
+      image: phoneImage,
+      description: 'this is a little description of the 7 plus',
+      quantity: 3,
+    },
+  ]
   return (
     <div className={styles.container}>
       {itemsInCart.length === 0 ? (
@@ -20,7 +36,15 @@ const Cart = () => {
         </div>
       ) : (
         <div className={styles.cart}>
-          <h6>some items in cart</h6>
+          <div className={styles.shoppingCart}>
+            <h5>Shopping Cart</h5>
+            <CartItem cartItem={itemsInCart} />
+          </div>
+          <div className={styles.checkout}>
+            <h6>Subtotal (2 items):</h6>
+            <p>$300.45</p>
+            <button>Proceed to checkout</button>
+          </div>
         </div>
       )}
     </div>
