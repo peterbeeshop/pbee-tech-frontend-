00e5694042ac8db69159fc0ee8d4d1ebeaf96fe9
peter muyambango <muyambangopeter@gmail.com>
2024-02-14 13:39:08 +0200

    added redux-persist so that the app data can persist in the browser

--- src/index.tsx
+++ src/index.tsx
@@ -3,8 +3,9 @@ import ReactDOM from 'react-dom/client'
 import { RouterProvider } from 'react-router-dom'
 import { ToastContainer } from 'react-toastify'
 import { router } from './App'
-import store from './store'
+import store, { persistor } from './store'
 import { Provider } from 'react-redux'
+import { PersistGate } from 'redux-persist/integration/react'
 import 'react-toastify/dist/ReactToastify.css'
 import './index.css'
 
@@ -12,8 +13,10 @@ const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <RouterProvider router={router} />
-      <ToastContainer position="bottom-center" />
+      <PersistGate loading={null} persistor={persistor}>
+        <RouterProvider router={router} />
+        <ToastContainer position="bottom-center" />
+      </PersistGate>
     </Provider>
   </React.StrictMode>,
 )
--- src/store/index.ts
+++ src/store/index.ts
@@ -1,11 +1,45 @@
-import { configureStore } from '@reduxjs/toolkit'
+import { configureStore, combineReducers } from '@reduxjs/toolkit'
+import {
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+  persistStore,
+} from 'redux-persist'
+import storage from 'redux-persist/lib/storage' // defaults to localStorage for web
 import userReducer from './user'
 
-const store = configureStore({
-  reducer: {
-    user: userReducer,
-  },
+const rootReducer = combineReducers({
+  user: userReducer,
 })
+
+/**
+ * Configuration for redux-persist
+ */
+const persistConfig = {
+  key: 'pbeeTech', // The key used to store it in LocalStorage
+  storage,
+}
+
+const persistedReducer = persistReducer(persistConfig, rootReducer)
+
+const store = configureStore({
+  reducer: persistedReducer,
+  devTools: process.env.NODE_ENV !== 'production',
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        //it helps to catch and log any non-serializable actions.
+        //"non-serializable actions" refer to actions whose data cannot be easily converted to a JSON format.
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER], //action types that should be ignored during the serializable check.
+      },
+    }),
+})
+
+export const persistor = persistStore(store)
 
 // Infer the `RootState` and `AppDispatch` types from the store itself
 export type RootState = ReturnType<typeof store.getState>
