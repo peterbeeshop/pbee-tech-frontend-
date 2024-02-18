e0f0c65f27a76fffff462d1e42d0bca98eac81cc
peter muyambango <muyambangopeter@gmail.com>
2024-02-18 21:36:02 +0200

    user is able to edit their password

--- src/pages/account/security/change-password/index.tsx
+++ src/pages/account/security/change-password/index.tsx
@@ -1,7 +1,30 @@
-import { Link } from 'react-router-dom'
+import { useState } from 'react'
+import { Link, useNavigate } from 'react-router-dom'
+import { toast } from 'react-toastify'
+import { editUserActions } from '../../../../store/account/security'
+import { useAppDispatch } from '../../../../store/hooks'
 import styles from './index.module.scss'
 
 const ChangePassword = () => {
+  const dispatch = useAppDispatch()
+  const navigate = useNavigate()
+  const [oldPassword, setOldPassword] = useState('')
+  const [newPassword, setNewPassword] = useState('')
+  const [confirmNewPassword, setConfirmNewPassword] = useState('')
+
+  const handleChangePassword = () => {
+    if (newPassword === confirmNewPassword) {
+      dispatch(
+        editUserActions.changeUserPassword({
+          oldPassword,
+          newPassword,
+          onSuccess: () => navigate(-1),
+        }),
+      )
+    } else {
+      toast.error('Your new password and confirm new password did not match!')
+    }
+  }
   return (
     <div className={styles.wrapper}>
       <div className={styles.container}>
@@ -23,17 +46,32 @@ const ChangePassword = () => {
           <p>Use the form below to change your password.</p>
           <div>
             <h6>Current Password</h6>
-            <input type="text" placeholder="current password" />
+            <input
+              type="text"
+              onChange={(e) => setOldPassword(e.target.value)}
+              value={oldPassword}
+              placeholder="current password"
+            />
           </div>
           <div>
             <h6>New Password</h6>
-            <input type="text" placeholder="Your new password" />
+            <input
+              type="text"
+              onChange={(e) => setNewPassword(e.target.value)}
+              value={newPassword}
+              placeholder="Your new password"
+            />
           </div>
           <div>
             <h6>Confirm Password</h6>
-            <input type="text" placeholder="confirm new password" />
+            <input
+              type="text"
+              onChange={(e) => setConfirmNewPassword(e.target.value)}
+              value={confirmNewPassword}
+              placeholder="confirm new password"
+            />
           </div>
-          <button>Save changes</button>
+          <button onClick={handleChangePassword}>Save changes</button>
         </div>
       </div>
     </div>
